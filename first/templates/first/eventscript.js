310cbd3da174b2d1cf839110267f5cd54d7aa0a1
MS_L <msl9204@gmail.com>
2020-01-13 22:58:29 +0900

    nav-complete

--- first/templates/first/eventscript.js
+++ first/templates/first/eventscript.js
@@ -1,6 +1,7 @@
 const $button = $('.img-button')
 const $menu = $('#drop_id')
 const $about = $('.about-hidden')
+const $post = $('.post')
 
 
 $($button).mouseup(function(){
@@ -14,28 +15,42 @@ $('#drop_id a:nth-child(2)').mouseup(function(){
     $($about).addClass("is-active")
 });
 
+$('#drop_id a:nth-child(3)').mouseup(function(){
+    $('.is-active').removeClass("is-active")
+    $($post).addClass("is-active")
+});
 
 
-$(document).mouseup(e => {
-    if (!$button.is(e.target) // if the target of the click isn't the container...
-    && $menu.has(e.target).length === 0) // ... nor a descendant of the container
-    {
-        // $menu.removeClass('is-active');
+
+$(document).mouseup(function(e){
+    let target = $(e.target)
+    if(target.is($button)){
+
+    }
+    else if($menu.has(target).length === 1){
+
+    }
+
+    else if($about.has(target).length === 1){
+
+    }
+
+    else if($post.has(target).length === 1){
+
+    }
+
+    else{
         $('.is-active').removeClass("is-active")
-
     }
-
-    if(!$button.is(e.target)){
-
-        if($menu.has(e.target).length === 0){
-            $('.is-active').removeClass("is-active")
-        }
-        if($about.has(e.target).length === 0){
-            $('.is-active').removeClass("is-active")
-        }
-
-    }
-
 
 });
 
+$('.about-hidden a:first-child').click(function(){
+    $('.is-active').removeClass("is-active")
+    $menu.addClass("is-active")
+})
+
+$('.post a:first-child').click(function(){
+    $('.is-active').removeClass("is-active")
+    $menu.addClass("is-active")
+})
